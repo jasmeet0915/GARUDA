6228dc7c777ab607a7633e6a3aa51673e17b5c99
Jasmeet Singh <jasmeet0915@gmail.com>
2024-05-20 01:32:20 +0530

    Added hunter video streaming

--- scripts/run_video_streams.bash
+++ scripts/run_video_streams.bash
@@ -9,6 +9,9 @@ VIDEO_PATH3="$PROJECT_MEDIA_PATH/Cattle_Drone_Footage.mp4"
 VIDEO_NAME3="Cattle_Drone_Footage"
 VIDEO_PATH4="$PROJECT_MEDIA_PATH/Elephant.mkv"
 VIDEO_NAME4="Elephant"
+VIDEO_PATH5="$PROJECT_MEDIA_PATH/Hunter.mp4"
+VIDEO_NAME5="Hunter"
+
 
 # Name of the tmux session
 SESSION_NAME="video_streams"
@@ -34,6 +37,21 @@ tmux send-keys -t 0 "ffmpeg -re -stream_loop -1 -i $VIDEO_PATH1 -c copy -f rtsp 
 tmux send-keys -t 1 "ffmpeg -re -stream_loop -1 -i $VIDEO_PATH2 -c copy -f rtsp rtsp://localhost:8554/$VIDEO_NAME2" C-m
 tmux send-keys -t 2 "ffmpeg -re -stream_loop -1 -i $VIDEO_PATH3 -c copy -f rtsp rtsp://localhost:8554/$VIDEO_NAME3" C-m
 tmux send-keys -t 3 "ffmpeg -re -stream_loop -1 -i $VIDEO_PATH4 -c copy -f rtsp rtsp://localhost:8554/$VIDEO_NAME4" C-m
+
+# Create a new window for video streams
+tmux new-window -t $SESSION_NAME -n "Video_Streams"
+
+# Sleep for 2 seconds and allow the server to start
+sleep 2
+
+# Split the window into 4 panes
+tmux split-window -h
+tmux split-window -v
+tmux select-pane -t 0
+tmux split-window -v
+
+# Start the virutal environment and Launch the Python scripts in each pane
+tmux send-keys -t 0 "ffmpeg -re -stream_loop -1 -i $VIDEO_PATH5 -c copy -f rtsp rtsp://localhost:8554/$VIDEO_NAME5" C-m
 
 # Attach to the tmux session
 tmux attach-session -t $SESSION_NAME
